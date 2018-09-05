28f5cd66d77c50a169bc7b2ef24cfe8e84bdbb31
Trace Larson <trace.larson4917@gmail.com>
2018-09-04 23:30:40 -0500

    created server in bin/www file. Created app.js and added in mongodb configurations and bodyparser configurations

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+
+// Configure Body Parser
+app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+// Connect to MongoDB Databse
+mongoose.connect(`mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}`)
+
+// Get default connection
+var db = mongoose.connection
+
+// Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
+// Bind connection to connection event
+db.once('open', () => console.log('DATABASE CONNECTED SUCCESSFULLY'))
+
+
+// ROUTES
+// TODO: create routes
+
+
+module.exports = app
---
+++ bin/www.js
@@ -0,0 +1,17 @@
+// Environment Variables
+var NODE_ENV = process.env.NODE_ENV || 'development'
+
+if (NODE_ENV === 'development') {
+	require('dotenv').load()
+}
+
+const app = require('../app.js')
+const http = require('http')
+
+const port = process.env.PORT || 5000
+app.set('port', port)
+
+const server = http.createServer(app)
+server.listen(port)
+server.on('listening', () => console.log('Server running on port ' + port))
+
