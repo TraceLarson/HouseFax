a7bcd7ea01ec15033cc555089dd35c24b2d1ef7b
Trace Larson <trace.larson4917@gmail.com>
2018-09-10 23:05:27 -0500

    added bootstrap dependencies as well as uuid for development purposes and react-transition-group for transition animations

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>Hello</h1>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default App
