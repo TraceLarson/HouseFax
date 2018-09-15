a22735dff90a46523d9f30ff674b2c3644c8b522
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 20:55:25 -0500

    Added routes for HomePage, LoginPage, RegisterPage, ProfilePage, ResultsPage, DetailsPage and imports to App.js, Also implemented NavigationHeader and Footer.

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,28 @@ import './App.css'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import ProfilePage from './pages/ProfilePage'
+import ResultsPage from './pages/ResultsPage'
+import DetailsPage from './pages/DetailsPage'
+
+import NavigationHeader from './components/NavigationHeader'
+import Footer from './components/Footer'
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div className="App">
-					<HomePage />
+					<NavigationHeader />
+					<Route exact path={'/'} component={HomePage} />
+					<Route path={'/Login'} component={LoginPage} />
+					<Route path={'/Register'} component={RegisterPage} />
+					<Route path={'/Profile'} component={ProfilePage} />
+					<Route path={'/Results'} component={ResultsPage} />
+					<Route path={'/Details'} component={DetailsPage} />
+					<Footer />
 				</div>
 			</Router>
 		);
