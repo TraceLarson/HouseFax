03a47c2d57da8c5b7dc265e8695dd49bf3e27be3
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 20:33:02 -0500

    created Navigation class component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Navigation extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Navigation component</h1>
+			</div>
+		);
+	}
+}
+
+export default Navigation;
