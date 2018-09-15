3cd0f6af4afa37a8088c4343c9cbd00b830823d8
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 20:22:16 -0500

    wrapped App.js div with Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>Hello</h1>
-			</div>
+			<Router>
+				<div className="App">
+					<h1>Hello</h1>
+				</div>
+			</Router>
 		);
 	}
 }
