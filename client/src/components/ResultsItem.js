8595eb1569c301fc3e523425dd1ec98ec9940780
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 20:36:51 -0500

    Created ResultsItem class component

---
+++ client/src/components/ResultsItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ResultsItem extends Component {
+	render() {
+		return (
+			<div>
+				<h1>ResultsItem Component</h1>
+			</div>
+		);
+	}
+}
+
+export default ResultsItem;
