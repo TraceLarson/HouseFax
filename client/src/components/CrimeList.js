461aa2d0a78dfd5801f9b80b2b24aa7a6b2be0ec
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 20:41:03 -0500

    Created CrimeList class component

---
+++ client/src/components/CrimeList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CrimeList extends Component {
+	render() {
+		return (
+			<div>
+				<h1>CrimeList Component</h1>
+			</div>
+		);
+	}
+}
+
+export default CrimeList;
