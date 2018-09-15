ff48967833578483c5d2f3d8ae461efa3b3a7926
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 20:38:23 -0500

    Created FamilyFriendlyRating class component

---
+++ client/src/components/FamilyFriendlyRating.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FamilyFriendlyRating extends Component {
+	render() {
+		return (
+			<div>
+				<h1>FamilyFriendlyRating Component</h1>
+			</div>
+		);
+	}
+}
+
+export default FamilyFriendlyRating;
