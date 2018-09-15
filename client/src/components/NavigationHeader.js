297e0f5731439697fb0f8c54b69bc09145bfe102
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 21:16:00 -0500

    Inserted base navbar to NavigationHeader

--- client/src/components/NavigationHeader.js
+++ client/src/components/NavigationHeader.js
@@ -13,10 +13,53 @@ import {
 	DropdownItem } from 'reactstrap'
 
 class NavigationHeader extends Component {
+	state = {
+		isOpen: false
+	}
+
+	toggle = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+
+
 	render() {
 		return (
 			<div>
-				<h1>Navigation component</h1>
+				<Navbar color={'light'} light expand={'md'} >
+					<NavbarBrand href={'/'}>HomeFax</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav>
+							<NavItem>
+								<NavLink href={'/Login'}>Login</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href={'/Register'} >Register</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href={'/Details'}>Details</NavLink>
+							</NavItem>
+							<UncontrolledDropdown nav inNavBar>
+								<DropdownToggle nav caret>
+									Options
+								</DropdownToggle>
+								<DropdownMenu right>
+									<DropdownItem>
+										Option 1
+									</DropdownItem>
+									<DropdownItem>
+										Option 2
+									</DropdownItem>
+									<DropdownItem divider>
+										Reset
+									</DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+						</Nav>
+					</Collapse>
+				</Navbar>
 			</div>
 		);
 	}
