9868d13a9dde31a3f400b1ee0eaf9bd1c8c9fac7
Trace Larson <trace.larson4917@gmail.com>
2018-09-14 20:35:50 -0500

    Created RegisterForm class component

---
+++ client/src/components/RegisterForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class RegisterForm extends Component {
+	render() {
+		return (
+			<div>
+				<h1>RegisterForm Component</h1>
+			</div>
+		);
+	}
+}
+
+export default RegisterForm;
