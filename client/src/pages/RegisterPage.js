acdac76d68fe30177ce91e700c65e56a30e25d70
Trace Larson <trace.larson4917@gmail.com>
2018-09-15 22:18:07 -0500

    imported RegisterForm to RegisterPage

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 
+import RegisterForm from '../components/RegisterForm'
+
 class RegisterPage extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Register page</h1>
+				<RegisterForm />
 			</div>
 		);
 	}
