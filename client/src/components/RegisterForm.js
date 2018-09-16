1e9c90b94fea4d2190e70f877ce2625c79b024e1
Trace Larson <trace.larson4917@gmail.com>
2018-09-15 22:28:14 -0500

    Added form inputs for register page

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react';
+import {
+	Container,
+	Button,
+	Form,
+	FormGroup,
+	Label,
+	Input,
+	FormText
+} from 'reactstrap'
 
 class RegisterForm extends Component {
 	render() {
 		return (
-			<div>
-				<h1>RegisterForm Component</h1>
-			</div>
+			<Container>
+				<Form>
+					<FormGroup>
+						<Label for={'firstname'}> </Label>
+						<Input type={'text'} name={'firstname'} placeholder={'First name'}/>
+					</FormGroup>
+					<FormGroup>
+						<Label for={'lastname'}> </Label>
+						<Input type={'text'} name={'lastname'} placeholder={'Last name'}/>
+					</FormGroup>
+					<FormGroup>
+						<Label for={'email'}> </Label>
+						<Input type={'email'} name={'email'} placeholder={'Email address'}/>
+					</FormGroup>
+					<FormGroup>
+						<Label for={'password'}> </Label>
+						<Input type={'password'} name={'password'} placeholder={'Password'}/>
+					</FormGroup>
+					<FormGroup>
+						<Label for={'repeatpassword'}> </Label>
+						<Input type={'password'} name={'repeatpassword'} placeholder={'Repeat password'}/>
+					</FormGroup>
+				</Form>
+			</Container>
 		);
 	}
 }
