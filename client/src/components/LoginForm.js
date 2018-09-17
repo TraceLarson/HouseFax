93f8657f620171a0b5dec536dc87a2bb0a1a5705
Trace Larson <trace.larson4917@gmail.com>
2018-09-16 21:16:37 -0500

    imported reactstrap components, created remember me checkbox, forgot password link, and sign up paragraph. Changed input username to email, hid labels. Added container for form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,19 +1,38 @@
 import React, {Component} from 'react';
+import {
+	Container,
+	Button,
+	Form,
+	FormGroup,
+	Label,
+	Input,
+} from 'reactstrap'
 
 
 class LoginForm extends Component {
 	render() {
 		return (
-			<div>
+			<Container className={'text-center'}>
 				<h1>HouseFax</h1>
-				<form action={''} method={'POST'}>
-					<label htmlFor={'username'}>Username</label>
-					<input name={'username'} type={'text'} placeholder={'username'}/>
-					<label htmlFor={'password'}>Password</label>
-					<input name={'password'} type={'password'} placeholder={'Password'}/>
-					<button type={'submit'}>Login</button>
-				</form>
-			</div>
+				<Form action={''} method={'POST'}>
+					<FormGroup>
+						<Label className={'d-none'} htmlFor={'email'}>Email</Label>
+						<Input name={'email'} type={'email'} placeholder={'email'}/>
+					</FormGroup>
+					<FormGroup>
+						<Label className={'d-none'} htmlFor={'password'}>Password</Label>
+						<Input name={'password'} type={'password'} placeholder={'Password'}/>
+					</FormGroup>
+					<Button type={'submit'}>Login</Button>
+					<FormGroup>
+						<Label>
+							<Input type={'checkbox'} name={'rememberCheck'}/>
+							Remember Me <a href={'#'}>Forgot password?</a>
+						</Label>
+					</FormGroup>
+				</Form>
+				<p>Don't have an account?  <a href={'/Register'}>Sign Up</a> </p>
+			</Container>
 		);
 	}
 }
