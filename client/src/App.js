2cbd68a5932edbe5ebde2f475143dc597d86efa5
Trace Larson <trace.larson4917@gmail.com>
2018-09-19 21:40:13 -0500

    fixed error in App.js. Changed setAuthToken import from ",/setAuthToken" to "./set.....

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux'
 import store from './store'
 
 import jwt_decode from 'jwt-decode'
-import setAuthToken from ',/setAuthToken'
+import setAuthToken from './setAuthToken'
 import { setCurrentUser, logoutUser} from './actions/authentication'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
