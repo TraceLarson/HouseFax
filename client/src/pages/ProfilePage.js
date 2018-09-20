12b46a1ba76fbb7480175829640c708bd17b56ed
Trace Larson <trace.larson4917@gmail.com>
2018-09-19 22:15:06 -0500

    added axios call to /user/me to get token holder info

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 
 class ProfilePage extends Component {
+
+	state = {
+		user: {}
+	}
+
+	componentWillMount() {
+		axios.get('/user/me')
+			.then(response => {
+				console.log(response)
+			})
+	}
+
 	render() {
 		return (
 			<div>
