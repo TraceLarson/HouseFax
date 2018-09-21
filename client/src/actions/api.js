b9ac9d3899f3e370ac37d32598e5e38ac40a626a
Trace Larson <trace.larson4917@gmail.com>
2018-09-20 21:26:18 -0500

    changed getListings payload to response.data.bundle instead of response.data

--- client/src/actions/api.js
+++ client/src/actions/api.js
@@ -4,11 +4,11 @@ import {GET_ERRORS, GET_LISTINGS} from './types'
 export const getListings = (query, history) => dispatch => {
 	axios.post('/api/bridge',{location: query})
 		.then(response => {
-			console.log('listing data: ', response.data)
+			console.log('listing data: ', response.data.bundle)
 			history.push('/Results')
 			dispatch({
 				type: GET_LISTINGS,
-				payload: response.data
+				payload: response.data.bundle
 			})
 		})
 		.catch(err => {
