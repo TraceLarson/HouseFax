b200f7f33d98938a0cd5933784296d13e70c9e49
Trace Larson <trace.larson4917@gmail.com>
2018-09-20 22:12:18 -0500

    ResultsPage takes listings array and maps it to ResultsItem component

--- client/src/pages/ResultsPage.js
+++ client/src/pages/ResultsPage.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 import Search from '../components/Search'
 import ResultsItem from '../components/ResultsItem'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {withRouter} from 'react-router-dom'
 
 class ResultsPage extends Component {
+
+
 	render() {
+		let resultItems = this.props.listings.map((listing => {
+			return (
+				<ResultsItem
+					key={listing.ListingKey}
+					id={listing.ListingId}
+					price={listing.ListPrice}
+					address={listing.UnparsedAddress}
+					city={listing.City}
+					state={listing.StateOrProvince}
+					imageSRC={listing.Media[0].MediaURL}
+				/>
+			)
+		})
 		return (
 			<div>
 				<p>Results Page</p>
@@ -14,4 +32,12 @@ class ResultsPage extends Component {
 	}
 }
 
-export default ResultsPage;
+ResultsPage.propTypes = {
+
+}
+
+const mapStateToProps = state => {
+	listings: state.listings
+}
+
+export default connect(mapStateToProps)(withRouter(ResultsPage));
