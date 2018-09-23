4335d506077f1ac6e63e0298688210b9daf67ad4
Trace Larson <trace.larson4917@gmail.com>
2018-09-23 15:47:34 -0500

    Created AgentContactForm class component

---
+++ client/src/components/AgentContactForm.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class AgentContactForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+AgentContactForm.propTypes = {};
+
+export default AgentContactForm;
