1381b79ca72ea616651910403e83422639a80bca
Trace Larson <trace.larson4917@gmail.com>
2018-09-23 15:53:08 -0500

    Createdd PropertyDetails component

---
+++ client/src/components/PropertyDetails.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class PropertyDetails extends Component {
+    render() {
+        return (
+            <div>
+                <p>PropertyDetails Component</p>
+            </div>
+        );
+    }
+}
+
+PropertyDetails.propTypes = {};
+
+export default PropertyDetails;
