54b4bd39af0731cbb8bb46361a4b72449a4997d5
Trace Larson <trace.larson4917@gmail.com>
2018-09-23 15:33:11 -0500

    importing module to DetailsPage

--- client/src/pages/DetailsPage.js
+++ client/src/pages/DetailsPage.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom'
+import {
+	Container
+} from 'reactstrap'
+
+import Detail from '../components/Detail'
 
 class DetailsPage extends Component {
 	render() {
 		return (
-			<div>
-				<p>Details Page</p>
-			</div>
+			<Container>
+				<h1>Details Page</h1>
+				<Detail />
+			</Container>
 		);
 	}
 }
 
-export default DetailsPage;
+export default withRouter(DetailsPage);
