5e28da34a722168d0e09939362c099bdb639ecf4
Trace Larson <trace.larson4917@gmail.com>
2018-09-23 15:45:34 -0500

    added component class to DetailsImage component file

---
+++ client/src/components/DetailsImage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class DetailsImage extends Component {
+    render() {
+        return (
+            <div>
+                <p>DetailsImage component</p>
+            </div>
+        );
+    }
+}
+
+DetailsImage.propTypes = {};
+
+export default DetailsImage;
