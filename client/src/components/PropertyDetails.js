f676290f77be1200931666b543dac02c4673bc3d
Trace Larson <trace.larson4917@gmail.com>
2018-09-23 20:05:55 -0500

    added container inside property details, two unordered lists, paragraph for description

--- client/src/components/PropertyDetails.js
+++ client/src/components/PropertyDetails.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {
+	Container
+} from 'reactstrap'
 
 class PropertyDetails extends Component {
-    render() {
-        return (
-            <div>
-                <p>PropertyDetails Component</p>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<h2>Property Details</h2>
+				<hr/>
+				<Container>
+					<div>
+						<ul>
+							<li>Home type</li>
+							<li> Beds</li>
+							<li> Bath</li>
+							<li>Built in</li>
+						</ul>
+						<ul>
+							<li> sq ft</li>
+							<li>$ /sqft</li>
+							<li>$ /month HOA</li>
+						</ul>
+					</div>
+					<p>Description</p>
+				</Container>
+			</div>
+		);
+	}
 }
 
 PropertyDetails.propTypes = {};
