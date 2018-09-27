04b40a15079daf7082a263ebc16b5c1965c3681a
Trace Larson <trace.larson4917@gmail.com>
2018-09-26 23:55:39 -0500

    added crimeListReducer reducer

--- client/src/reducers/crimeListReducer.js
+++ client/src/reducers/crimeListReducer.js
@@ -7,7 +7,9 @@ const initialState = {
 export default function (state = initialState, action) {
 	switch (action.type) {
 		case GET_CRIMES:
-			return console.log('crimeListReducer running') /*Object.assign({}, state, action.payload)*/
+			return (
+				Object.assign({}, state, action.payload)
+			)
 		default:
 			return state
 	}
