4b6f4de696c7799fd4790f335f9c2c7888415c84
Trace Larson <trace.larson4917@gmail.com>
2018-09-27 23:01:17 -0500

    calculated days and hrs from most recent crime report to now

--- client/src/components/CrimeCard.js
+++ client/src/components/CrimeCard.js
@@ -6,15 +6,16 @@ import {
 class CrimeCard extends Component {
 	render() {
 		const now  = new Date()
-		const then = this.props.occDateTime
-
-		// const time = Math.abs(now - then)
+		let then = this.props.occDateTime.toString()
+		let dateThen = new Date(`${then}`)
+		let timeDiffDay = (((((now - dateThen) / 1000)/60)/60)/24).toFixed(0)
+		let timeDiffHr = (((((now - dateThen) / 1000)/60)/60)%24).toFixed(0)
 
 		return (
 			<div className={'card'}>
 				<div className={'card-body'}>
 					<strong className={'card-title'}>{this.props.crimeType}</strong>
-					<p className={'card-text'}>{then}</p>
+					<p className={'card-text'}>{`${timeDiffDay} days and ${timeDiffHr} hours ago`}</p>
 					<Button><strong>View On Map</strong></Button>
 				</div>
 			</div>
