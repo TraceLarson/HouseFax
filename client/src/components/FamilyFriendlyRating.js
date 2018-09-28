be6cab85ec28c73ed70d14fad301d2ae587004ad
Trace Larson <trace.larson4917@gmail.com>
2018-09-27 19:49:41 -0500

    testing circular progress bars for Family Friendly rating

--- client/src/components/FamilyFriendlyRating.js
+++ client/src/components/FamilyFriendlyRating.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
+import CircularProgressbar from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+
+
 
 class FamilyFriendlyRating extends Component {
+	state = {
+		percentage: 75
+	}
+
+
 	render() {
 		return (
-			<div>
-				<p>FamilyFriendlyRating Component</p>
+			<div className={'family-friendly-rating-container'}>
+				<CircularProgressbar
+					percentage={this.state.percentage}
+					text={`${this.state.percentage}`}
+					initialAnimation={true}
+					styles={{
+						path: { stroke: `rgba(62, 152, 199, ${this.state.percentage / 100})` },
+						text: { fill: '#f88', fontSize: '16px' },
+					}}
+				/>
 			</div>
-		);
+		)
 	}
 }
 
