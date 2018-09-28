f56608afef2bbf6b60c3c18a5df3008acef6d8c1
Trace Larson <trace.larson4917@gmail.com>
2018-09-27 21:56:15 -0500

    limited recent crimes to 4 cards, playing with padding on crime-container class

--- client/src/components/RecentCrimeReports.js
+++ client/src/components/RecentCrimeReports.js
@@ -3,27 +3,34 @@ import PropTypes from 'prop-types';
 import CrimeCard from "./CrimeCard";
 
 class RecentCrimeReports extends Component {
-    render() {
+	render() {
 
-        let crimes = Object.keys(this.props.crimeList).map(key =>{
-            let crime = this.props.crimeList[key]
-            return(
-	            <CrimeCard key={key}
-                           latitude={crime.latitude}
-                           longitude={crime.longitude}
-                           occDateTime={crime.occ_date_time}
-                           crimeType={crime.crime_type}
-                           familyViolence={crime.family_violence}
-	            />
-            )
-        })
+		let crimes = Object.keys(this.props.crimeList).map(key => {
+			let crime = this.props.crimeList[key]
+			if (key < 4) {
+				return (
+					<CrimeCard key={key}
+					           latitude={crime.latitude}
+					           longitude={crime.longitude}
+					           occDateTime={crime.occ_date_time}
+					           crimeType={crime.crime_type}
+					           familyViolence={crime.family_violence}
+					/>
+				)
+			} else {
+				return ''
+			}
+		})
 
-        return (
-            <div className={'crime-cards-container'}>
-                {crimes}
-            </div>
-        );
-    }
+		return (
+			<div>
+				<h3>Most Recent Reports</h3>
+				<div className={'crime-cards-container'}>
+					{crimes}
+				</div>
+			</div>
+		);
+	}
 }
 
 RecentCrimeReports.propTypes = {};
