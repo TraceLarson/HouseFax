8d26ed8a432c49169dcce89d11d06ef3fcf88f96
Trace Larson <trace.larson4917@gmail.com>
2018-09-27 22:33:54 -0500

    added button to crime card to bring up modal map

--- client/src/components/CrimeCard.js
+++ client/src/components/CrimeCard.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+import {
+	Button
+} from 'reactstrap'
 
 class CrimeCard extends Component {
 	render() {
+		const now  = new Date()
+		const then = this.props.occDateTime
+
+		// const time = Math.abs(now - then)
+
 		return (
 			<div className={'card'}>
 				<div className={'card-body'}>
-				<strong className={'card-title'}>{this.props.crimeType}</strong>
-				<p className={'card-text'}>{this.props.occDateTime}</p>
+					<strong className={'card-title'}>{this.props.crimeType}</strong>
+					<p className={'card-text'}>{then}</p>
+					<Button><strong>View On Map</strong></Button>
 				</div>
 			</div>
 		);
