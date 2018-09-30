fb5fface10f43296574d7db5ba6d1b72ea17ad4c
Trace Larson <trace.larson4917@gmail.com>
2018-09-29 19:31:57 -0500

    added logic to check if tour url is available, if not then no tour button is displayed

--- client/src/components/DetailsImage.js
+++ client/src/components/DetailsImage.js
@@ -16,14 +16,15 @@ class DetailsImage extends Component {
 			 'http://fpoimg.com/760x466'
 		)
 
-		console.log(this.props.virtualTour)
-
+		const tourButton = this.props.virtualTour !== null ? (
+			<a className={'btn btn-secondary'} href={this.props.virtualTour} target={'_blank'} >Tour</a>
+		) : ''
 
 		return (
 			<Container className={'pl-0'}>
 				<div className={'details-image-container'}>
 					<img className={'img-fluid'} src={imageSrc} alt={'house'} width={'760px'} height={'466px'}/>
-					<a className={'btn btn-secondary'} href={this.props.virtualTour} target={'_blank'} >Tour</a>
+					{tourButton}
 				</div>
 			</Container>
 		);
