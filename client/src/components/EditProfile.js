58c92193d59dbe022a9ef39df07bf850c4cc01af
Trace Larson <trace.larson4917@gmail.com>
2018-10-08 12:36:11 -0500

    created EditProfile component

---
+++ client/src/components/EditProfile.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class EditProfile extends Component {
+
+	render() {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
+
+EditProfile.propTypes = {};
+
+export default EditProfile;
