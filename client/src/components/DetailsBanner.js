9647435248d4db0484a0cddb971a6b33bdf5f097
Trace Larson <trace.larson4917@gmail.com>
2018-10-10 23:52:53 -0500

    removed axios import from DetailsBanner component

--- client/src/components/DetailsBanner.js
+++ client/src/components/DetailsBanner.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
-import axios from 'axios'
 import {
 	Container,
 	Button
