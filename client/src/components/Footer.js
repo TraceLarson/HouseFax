42662fe7bb75a24f4725d3937431d343bb469ed3
Trace Larson <trace.larson4917@gmail.com>
2018-10-11 02:02:52 -0500

    added font awesome cdn link in /public index.html

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,10 +9,10 @@ class Footer extends Component {
 						<h5>Pages</h5>
 						<ul className="list-unstyled quick-links">
 							<li><a href="#"><i className="fa fa-angle-double-right"></i>Home</a></li>
+							<li><a href="#"><i className="fa fa-angle-double-right"></i>Login</a></li>
+							<li><a href="#"><i className="fa fa-angle-double-right"></i>Contact</a></li>
 							<li><a href="#"><i className="fa fa-angle-double-right"></i>About</a></li>
-							<li><a href="#"><i className="fa fa-angle-double-right"></i>FAQ</a></li>
-							<li><a href="#"><i className="fa fa-angle-double-right"></i>Get Started</a></li>
-							<li><a href="#"><i className="fa fa-angle-double-right"></i>Videos</a></li>
+							{/*<li><a href="#"><i className="fa fa-angle-double-right"></i>Videos</a></li>*/}
 						</ul>
 					</div>
 					<div className="col-xs-12 col-sm-5 col-md-3">
@@ -58,17 +58,6 @@ class Footer extends Component {
 							<li className="list-inline-item"><a href="#" target="_blank"><i
 								className="fa fa-envelope"></i></a></li>
 						</ul>
-					</div>
-					<hr/>
-				</div>
-				<div className="row">
-					<div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-						<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is
-							a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp,
-							Minneapolis, MN]</p>
-						<p className="h6">&copy All right Reversed.<a className="text-green ml-2"
-						                                              href="https://www.sunlimetech.com"
-						                                              target="_blank">Sunlimetech</a></p>
 					</div>
 					<hr/>
 				</div>
