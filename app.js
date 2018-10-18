ecca13c6942e2c8494c759186b04ea295e6f97de
TraceLarson <trace.larson4917@gmail.com>
2018-10-18 11:16:31 -0500

    edit default route for production build

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ if(process.env.NODE_ENV === 'production'){
 	// Set static folder
 	app.use(express.static('client/build'))
 
-	// app.get('*', (req, res) => {
-	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	// 	// res.sendFile(path.join(__dirname + '/client/build/index.html'))
-	// 	// res.sendFile(path.join(__dirname + '/../client/build/index.html'))
-	// })
+	app.get('/', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		// res.sendFile(path.join(__dirname + '/client/build/index.html'))
+		// res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+	})
 }
 
 // Passport Requirements
