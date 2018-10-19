987cb3874261efcb1c5d84afff07d0b7f8ea7daa
Trace Larson <trace.larson4917@gmail.com>
2018-10-18 21:09:34 -0500

    changed express.static to (client/build) instead of (./client/build)

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const property = require('./routes/property')
 // Serve static files if in production environment
 if(process.env.NODE_ENV === 'production'){
 	// Set static folder
-	// app.use(express.static('./client/build'))
-	app.use('/static', express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static('client/build'))
+	// app.use('/static', express.static(path.join(__dirname, 'client/build')));
 	app.get('/', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 		// res.sendFile(path.join(__dirname + '/client/build/index.html'))
