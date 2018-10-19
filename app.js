0e44f4f9eb4e71bc97be1e44808657f0a0017110
Trace Larson <trace.larson4917@gmail.com>
2018-10-19 10:19:26 -0500

    heroku troubleshooting...

--- app.js
+++ app.js
@@ -15,11 +15,15 @@ const property = require('./routes/property')
 
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'client/build/static')));
+	app.use(express.static(path.resolve(__dirname, 'client/build')));
+
+	app.use('/api', api)
+	app.use('/user', user)
+	app.use('/property', property)
 
 	// Handle React routing, return all requests to React app
 	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 	});
 }
 
