66ad53f1c5a73e2d7666a82f9f0b55b43018bb14
Trace Larson <trace.larson4917@gmail.com>
2018-10-19 11:05:10 -0500

    heroku troubleshooting...

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const path = require('path')
 const passport = require('passport')
 
 
+var NODE_ENV = process.env.NODE_ENV || 'development'
+
+if (NODE_ENV === 'development') {
+	require('dotenv').config()
+}
+const http = require('http')
+const port = process.env.PORT || 5000
+app.set('port', port)
+
+const server = http.createServer(app)
+server.listen(port)
+server.on('listening', () => console.log('Server running on port ' + port))
+
+
+
+
 
 // const index = require('./routes/index')
 const api = require('./routes/api')
