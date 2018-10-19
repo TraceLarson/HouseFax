825ec9d84092d49792f717ba8bbb18a301e8fe5b
Trace Larson <trace.larson4917@gmail.com>
2018-10-18 21:21:33 -0500

    changed production environment again

--- app.js
+++ app.js
@@ -18,12 +18,18 @@ const property = require('./routes/property')
 // Serve static files if in production environment
 if(process.env.NODE_ENV === 'production'){
 	// Set static folder
-	app.use(express.static('client/build'))
+	app.use(express.static(path.join(__dirname, 'client', 'build')))
+	// app.use(express.static('client/build/'))
 	// app.use('/static', express.static(path.join(__dirname, 'client/build')));
-	app.get('/', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-		// res.sendFile(path.join(__dirname + '/client/build/index.html'))
-	})
+
+	// app.get('/', (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	// 	// res.sendFile(path.join(__dirname + '/client/build/index.html'))
+	// })
+
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+	});
 }
 
 // Passport Requirements
